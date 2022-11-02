8403c2acd569c90f4387805f6327c12718b47dc2
Skye <skyemcclenny@tamu.edu>
2022-11-01 20:30:58 -0500

    Initial commit for Extra Features. Just blocking

--- src/Manager/Components/ExtraFeatures.js
+++ src/Manager/Components/ExtraFeatures.js
@@ -4,6 +4,10 @@ export const ExtraFeatures = () => {
   return (
     <div>
       <h1>Extra Features Page</h1>
+      {/* Enter Date */}
+      {/* Sales Report */}
+      {/* Excess Report */}
+      {/* What Sells Together */}
     </div>
   );
 };
