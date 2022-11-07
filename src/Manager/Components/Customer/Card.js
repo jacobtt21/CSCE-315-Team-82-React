c332261828a4923f81e00269e32e3732107bd6ac
jacobtt21 <jacobt1206@gmail.com>
2022-11-07 11:01:40 -0600

    menu is done

--- src/Manager/Components/Customer/Card.js
+++ src/Manager/Components/Customer/Card.js
@@ -1,14 +1,29 @@
 import React from "react";
-import Card from 'react-bootstrap/Card';
 
 export default function FoodCard({ item }) {
   return (
-    <div>
-      <Card>
-        <Card.Header>
-          <h3 className="d-inline align-middle">{item}</h3>
-        </Card.Header>
-      </Card>
-    </div>
+    <>
+      <div className="card">
+        <h1 className="name">{item.name}</h1>
+        <h1 className="name">${item.price}</h1>
+        <button
+        className="btn"
+        >
+          Click to add
+        </button>
+      </div>
+      <style jsx>{`
+        .name {
+          font-size: 18px;
+        }
+        .card {
+          margin: 25px auto;
+          width: 100%;
+          border: 3px solid #f8fafc;
+          padding: 10px;
+          box-shadow: 5px 10px 8px #e2e8f0;
+        }
+      `}</style>
+    </>
   )
 };
