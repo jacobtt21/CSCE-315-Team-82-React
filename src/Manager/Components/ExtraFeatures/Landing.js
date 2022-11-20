96dcc370ec6adbe74c9145a53eca58ead2a60646
Sam Hollenbeck <samhollenbeck@tamu.edu>
2022-11-20 14:49:04 -0600

    add links to all reports in extraFeatures

--- src/Manager/Components/ExtraFeatures/Landing.js
+++ src/Manager/Components/ExtraFeatures/Landing.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 export const ExtraFeatures = () => {
   return (
     <>
@@ -25,7 +24,7 @@ export const ExtraFeatures = () => {
             <div class="h-80 p-5 bg-light border rounded-3">
               <h2>Restock Report</h2>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lectus ex, elementum sed sagittis id, ultricies et magna. Morbi vel consequat est. Fusce tristique enim ut ipsum dictum, vel feugiat elit placerat.</p>
-              <button class="btn btn-outline-secondary" type="button">View Report</button>
+              <Link to="/ExtraFeatures/RestockReport"><button class="btn btn-outline-secondary" type="button">View Report</button></Link>
             </div>
           </div>
         </div>
@@ -35,7 +34,7 @@ export const ExtraFeatures = () => {
             <div class="h-80 p-5 bg-light border rounded-3">
               <h2>Excess Report</h2>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lectus ex, elementum sed sagittis id, ultricies et magna. Morbi vel consequat est. Fusce tristique enim ut ipsum dictum, vel feugiat elit placerat.</p>
-              <button class="btn btn-outline-secondary" type="button">View Report</button>
+              <Link to="/ExtraFeatures/ExcessReport"><button class="btn btn-outline-secondary" type="button">View Report</button></Link>
             </div>
           </div>
 
@@ -43,7 +42,7 @@ export const ExtraFeatures = () => {
             <div class="h-80 p-5 bg-light border rounded-3">
               <h2>Pairings Report</h2>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lectus ex, elementum sed sagittis id, ultricies et magna. Morbi vel consequat est. Fusce tristique enim ut ipsum dictum, vel feugiat elit placerat.</p>
-              <button class="btn btn-outline-secondary" type="button">View Report</button>
+              <Link to="/ExtraFeatures/PairingsReport"><button class="btn btn-outline-secondary" type="button">View Report</button></Link>
             </div>
           </div>
         </div>
