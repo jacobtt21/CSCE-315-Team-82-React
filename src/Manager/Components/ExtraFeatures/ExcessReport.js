cf9a1883b8b50ba9763dd7cfda50442ed87aac91
brianchen12 <62766692+brianchen12@users.noreply.github.com>
2022-11-28 15:47:36 -0600

    added basic functionality to display excess items from 9/7/22 - 9/21/22

--- src/Manager/Components/ExtraFeatures/ExcessReport.js
+++ src/Manager/Components/ExtraFeatures/ExcessReport.js
@@ -11,28 +11,31 @@ import { faTrash, faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 import { useParams, Link } from "react-router-dom";
 import Axios from 'axios';
 
+
 export const ExcessReport = () => {
   const [item, setItem] = useState([]);
 
-    useEffect(()=>{
-      Axios.get(process.env.REACT_APP_API_URL+`/get-excess-report/9-07-22/9-21-22`)
-        .then(res => {
-          const item = res.data;
-          setItem(item[0]);
-        })
-    },[]);
+  useEffect(()=>{
+    Axios.get(process.env.REACT_APP_API_URL+`/get-excess-report/9-07-22/9-21-22`)
+      .then(response => {
+        return response.data
+      })
+      .then(data => {
+        setItem(data)
+      })
+  },[]);
 
   return (
-    <>
-      <div class="col-md-6">
-        <div class="h-80 p-5 bg-light border rounded-3">
-          <h2>Sales Report</h2>
-          <Grid foods={item[0]} custom={true} />
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lectus ex, elementum sed sagittis id, ultricies et magna. Morbi vel consequat est. Fusce tristique enim ut ipsum dictum, vel feugiat elit placerat.</p>
-          <h2>item</h2>
-          <Link to="/ExtraFeatures/SalesReport"><button class="btn btn-outline-secondary" type="button">View Report</button></Link>
-        </div>
-      </div>
-    </>
+    <div>
+      {item.length > 0 && (
+        <ul>
+          {item.map(item => (
+            <li key={item.id}>{item.name}</li>
+          ))}
+        </ul>
+      )}
+    </div>
   )
-};
+}
+
+export default ExcessReport
