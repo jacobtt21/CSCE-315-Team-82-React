fa990783b2e866172e9c811491237e4c8743585c
Sam Hollenbeck <samhollenbeck@tamu.edu>
2022-12-06 16:12:15 -0600

    standard page header

--- src/Manager/Components/Home.js
+++ src/Manager/Components/Home.js
@@ -3,6 +3,11 @@ import React from "react";
 export const Home = () => {
   return (
     <div class="container py-4">
+    <header class="pb-3 mb-4 border-bottom">
+      <a href="/" class="d-flex align-items-center text-dark text-decoration-none">
+        <span class="fs-4">Home</span>
+      </a>
+    </header>
       <div class="p-5 mb-4 bg-light rounded-3">
         <div class="container-fluid py-5">
           <h1 class="display-5 fw-bold">Welcome back, {localStorage.getItem("full_name")}!</h1>
