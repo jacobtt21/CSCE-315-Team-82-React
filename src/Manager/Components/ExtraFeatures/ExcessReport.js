055b148ff3bc784768b1c682d8ab9b22d690fb6a
brianchen12 <62766692+brianchen12@users.noreply.github.com>
2022-12-05 21:38:16 -0600

    IT WORKSSSS

--- src/Manager/Components/ExtraFeatures/ExcessReport.js
+++ src/Manager/Components/ExtraFeatures/ExcessReport.js
@@ -4,6 +4,7 @@ import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
 import Alert from 'react-bootstrap/Alert';
 import InputGroup from 'react-bootstrap/InputGroup';
+// import Calendar from 'react-calendar';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash, faPenToSquare } from '@fortawesome/free-solid-svg-icons'
@@ -13,25 +14,29 @@ import Axios from 'axios';
 
 
 export const ExcessReport = () => {
-  const [item, setItem] = useState([]);
+  const [excessItems, setExcessItems] = useState([]);
   const [submitted, setSubmitted] = useState("");
-  const {start_date} = useParams();
-  const {end_date} = useParams();
+  const [start_date, setStartDate] = useState("");
+  const [end_date, setEndDate] = useState("");
 
   const onSubmitHandler = e => {
     e.preventDefault();
-    formData = new FormData();
-    formData.append("start_date", "9-07-22");
-    formData.append("end_date", "9-21-22");
-    Axios.get(process.env.REACT_APP_API_URL+`/get-excess-report`, formData, {})
+    Axios.get(process.env.REACT_APP_API_URL+`/get-excess-report/${start_date}/${end_date}`)
       .then((res) => {
         setSubmitted(true);
+        // const excessItems = res.data;
+        // console.log(excessItems);
+        return res.data
+      })
+      .then((data) => {
+        setExcessItems(data);
       })
       .catch((err) => {
         console.log(err);
       });
   }
-  
+
+
   const htmlForm = () => {
     return (
       <>
@@ -41,13 +46,13 @@ export const ExcessReport = () => {
             <Form autoComplete="off" onSubmit={onSubmitHandler}>
               <Form.Group className="mb-3">
                 <Form.Label>Start Date</Form.Label>
-                <Form.Control name="start date"/>
+                <Form.Control name="start date" onChange={(e) => setStartDate(e.target.value)} />
               </Form.Group>
 
               <Form.Group className="mb-3">
                 <Form.Label>End Date</Form.Label>
                 <InputGroup className="mb-3">
-                  <Form.Control name="end date"/>
+                  <Form.Control name="end date" onChange={(e) => setEndDate(e.target.value)} />
                 </InputGroup>
               </Form.Group>
 
@@ -63,14 +68,50 @@ export const ExcessReport = () => {
   }
 
   const htmlSubmitted = () => {
-    <>
-    </>
+    // return (
+    //   <>
+    //   <div class="submitted-container">
+    //     <h2>Items in excess: </h2>
+    //     {excessItems.map(excessItem => (
+    //       <li key={excessItem.item_id}>{excessItem.name}</li>
+    //     ))}
+    //     <Link to="/ExtraFeatures"><button class="btn btn-outline-secondary" type="button">View Report</button></Link>
+    //   </div>
+    //   <style jsx="true">{`
+    //     .submitted-container {
+    //       display: flex;
+    //       justify-content: center;
+    //       align-items: center;
+    //       height: 100vh;
+    //     }
+    //   `}</style>
+    //   </>
+    // ) 
+    return (
+      <Card style={{
+        backgroundColor: "#eee",
+        padding: 16,
+        minHeight: 70,
+        display: "flex",
+        alignItems: "center",
+        textAlign: "center"
+      }}>
+        <Card.Body>
+          <Card.Title>Items in Excess:</Card.Title>
+          <Card.Text>
+          {excessItems.map(excessItem => (
+            <li key={excessItem.item_id}>{excessItem.name}</li>
+          ))}
+          </Card.Text>
+          <Link to={"/ExtraFeatures"}><Button variant="primary">Done</Button></Link>
+        </Card.Body>
+      </Card>
+    );       
   }
 
-  // if (!submitted) {
-  //   return htmlSubmitted();
-  // } else {
-  //   return htmlForm();
-  // }
-  return htmlForm();
+  if (submitted) {
+    return htmlSubmitted();
+  } else {
+    return htmlForm();
+  }
 }
