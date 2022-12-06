74e414689614f9f8e54ef8cfd32bac17b6a1755e
Owen Moore <owen10@tamu.edu>
2022-12-05 23:32:19 -0600

    making reports

--- src/Manager/Components/ExtraFeatures/RestockReport.js
+++ src/Manager/Components/ExtraFeatures/RestockReport.js
@@ -1,6 +1,105 @@
+import React, { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import Alert from 'react-bootstrap/Alert';
+import InputGroup from 'react-bootstrap/InputGroup';
+// import Calendar from 'react-calendar';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash, faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+
+import { useParams, Link } from "react-router-dom";
+import Axios from 'axios';
+
+
 export const RestockReport = () => {
-  return (
-    <>
+  const [excessItems, setExcessItems] = useState([]);
+  const [submitted, setSubmitted] = useState("");
+
+  const onSubmitHandler = e => {
+    e.preventDefault();
+    Axios.get(process.env.REACT_APP_API_URL+`/get-restock-report/`)
+      .then((res) => {
+        setSubmitted(true);
+        // const excessItems = res.data;
+        // console.log(excessItems);
+        return res.data
+      })
+      .then((data) => {
+        setExcessItems(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+
+  const htmlForm = () => {
+    return (
+      <>
+      <div className="p-3">
+      <Card>
+          <Card.Body>
+            <Form autoComplete="off" onSubmit={onSubmitHandler}>
+
+              <Button type="submit" variant="primary">Submit</Button>
+              <Link to={"/Landing"}><Button variant="secondary">Back</Button></Link>
+
+            </Form>
+          </Card.Body>
+        </Card>
+      </div>
     </>
-  )
-};
+    ) 
+  }
+
+  const htmlSubmitted = () => {
+    // return (
+    //   <>
+    //   <div class="submitted-container">
+    //     <h2>Items in excess: </h2>
+    //     {excessItems.map(excessItem => (
+    //       <li key={excessItem.item_id}>{excessItem.name}</li>
+    //     ))}
+    //     <Link to="/ExtraFeatures"><button class="btn btn-outline-secondary" type="button">View Report</button></Link>
+    //   </div>
+    //   <style jsx="true">{`
+    //     .submitted-container {
+    //       display: flex;
+    //       justify-content: center;
+    //       align-items: center;
+    //       height: 100vh;
+    //     }
+    //   `}</style>
+    //   </>
+    // ) 
+    return (
+      <Card style={{
+        backgroundColor: "#eee",
+        padding: 16,
+        minHeight: 70,
+        display: "flex",
+        alignItems: "center",
+        textAlign: "center"
+      }}>
+        <Card.Body>
+          <Card.Title>Items That Need to be Restocked:</Card.Title>
+          <Card.Text>
+          {excessItems.map(excessItem => (
+            <li key={excessItem.item_id}> {excessItem.quantity} {excessItem.name} currently instock needs {excessItem.deficit} more </li>
+          ))}
+          </Card.Text>
+          <Link to={"/ExtraFeatures"}><Button variant="primary">Done</Button></Link>
+        </Card.Body>
+      </Card>
+    );       
+  }
+
+  if (submitted) {
+    return htmlSubmitted();
+  } else {
+    return htmlForm();
+  }
+}
+
--- src/Manager/Components/ExtraFeatures/SalesReport.js
+++ src/Manager/Components/ExtraFeatures/SalesReport.js
@@ -1,6 +1,125 @@
+import React, { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import Alert from 'react-bootstrap/Alert';
+import InputGroup from 'react-bootstrap/InputGroup';
+// import Calendar from 'react-calendar';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash, faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+
+import { useParams, Link } from "react-router-dom";
+import Axios from 'axios';
+
+
 export const SalesReport = () => {
+  const [Item1, setItem1] = useState([]);
+  const [item_name, setItemName] = useState([]);
+  const [submitted, setSubmitted] = useState("");
+  const [start_date, setStartDate] = useState("");
+  const [end_date, setEndDate] = useState("");
+
+  const onSubmitHandler = e => {
+    e.preventDefault();
+    Axios.get(process.env.REACT_APP_API_URL+`/get-pairings-report/${start_date}/${end_date}/${item_name}`)
+      .then((res) => {
+        setSubmitted(true);
+        // const excessItems = res.data;
+        // console.log(excessItems);
+        return res.data
+      })
+      .then((data) => {
+        setItem1(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+
+  const htmlForm = () => {
     return (
       <>
-      </>
-    )
-};
+      <div className="p-3">
+      <Card>
+          <Card.Body>
+            <Form autoComplete="off" onSubmit={onSubmitHandler}>
+              <Form.Group className="mb-3">
+                <Form.Label>Start Date</Form.Label>
+                <Form.Control name="start date" onChange={(e) => setStartDate(e.target.value)} />
+              </Form.Group>
+
+              <Form.Group className="mb-3">
+                <Form.Label>End Date</Form.Label>
+                <InputGroup className="mb-3">
+                  <Form.Control name="end date" onChange={(e) => setEndDate(e.target.value)} />
+                </InputGroup>
+              </Form.Group>
+
+              <Form.Group className="mb-3">
+                <Form.Label>Item Name</Form.Label>
+                <InputGroup className="mb-3">
+                  <Form.Control name="item name" onChange={(e) => setItemName(e.target.value)} />
+                </InputGroup>
+              </Form.Group>
+
+              <Button type="submit" variant="primary">Submit</Button>
+              <Link to={"/Landing"}><Button variant="secondary">Back</Button></Link>
+
+            </Form>
+          </Card.Body>
+        </Card>
+      </div>
+    </>
+    ) 
+  }
+
+  const htmlSubmitted = () => {
+    // return (
+    //   <>
+    //   <div class="submitted-container">
+    //     <h2>Items in excess: </h2>
+    //     {excessItems.map(excessItem => (
+    //       <li key={excessItem.item_id}>{excessItem.name}</li>
+    //     ))}
+    //     <Link to="/ExtraFeatures"><button class="btn btn-outline-secondary" type="button">View Report</button></Link>
+    //   </div>
+    //   <style jsx="true">{`
+    //     .submitted-container {
+    //       display: flex;
+    //       justify-content: center;
+    //       align-items: center;
+    //       height: 100vh;
+    //     }
+    //   `}</style>
+    //   </>
+    // ) 
+    return (
+      <Card style={{
+        backgroundColor: "#eee",
+        padding: 16,
+        minHeight: 70,
+        display: "flex",
+        alignItems: "center",
+        textAlign: "center"
+      }}>
+        <Card.Body>
+          <Card.Title>Number of {item_name} sold:</Card.Title>
+          <Card.Text>
+          {Item1.map(item1 => (
+            <li>{item1.cnt}</li>
+          ))}
+          </Card.Text>
+          <Link to={"/ExtraFeatures"}><Button variant="primary">Done</Button></Link>
+        </Card.Body>
+      </Card>
+    );       
+  }
+
+  if (submitted) {
+    return htmlSubmitted();
+  } else {
+    return htmlForm();
+  }
+}
