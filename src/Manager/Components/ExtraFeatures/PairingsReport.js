8c0f27e3abfb31fff34625144fa603c745a8cce6
Sam Hollenbeck <samhollenbeck@tamu.edu>
2022-12-06 23:34:53 -0600

    added format to pairings report

--- src/Manager/Components/ExtraFeatures/PairingsReport.js
+++ src/Manager/Components/ExtraFeatures/PairingsReport.js
@@ -2,11 +2,9 @@ import React, { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
+import Table from 'react-bootstrap/Table';
 import Alert from 'react-bootstrap/Alert';
 import InputGroup from 'react-bootstrap/InputGroup';
-import DatePicker from 'react-datepicker';
-
-import "react-datepicker/dist/react-datepicker.css";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash, faPenToSquare } from '@fortawesome/free-solid-svg-icons'
@@ -39,7 +37,6 @@ export const PairingsReport = () => {
       });
   }
 
-
   const htmlForm = () => {
     return (
       <>
@@ -49,14 +46,14 @@ export const PairingsReport = () => {
             <Form autoComplete="off" onSubmit={onSubmitHandler}>
               <Form.Group className="mb-3">
                 <Form.Label>Start Date</Form.Label>
-                <Form.Control name="start date" onChange={(e) => setStartDate(e.target.value)} />
-                <DatePicker selected={start_date} onChange={(e) => setStartDate(e)}/>
+                <Form.Control id="start_date" onChange={(e) => setStartDate(e.target.value)} />
+                {/* <DatePicker name="start_date" dateFormat="MM-DD-YYYY" onChange={(e) => setStartDate(e)}/> */}
               </Form.Group>
 
               <Form.Group className="mb-3">
                 <Form.Label>End Date</Form.Label>
-                <Form.Control name="end date" onChange={(e) => setEndDate(e.target.value)} />
-                <DatePicker selected={end_date} onChange={(e) => setEndDate(e)}/>
+                <Form.Control id="end_date" onChange={(e) => setEndDate(e.target.value)} />
+                {/* <DatePicker name="end_date" dateFormat="MM-DD-YYYY" onChange={(e) => setEndDate(e)}/> */}
               </Form.Group>
 
               <Button type="submit" variant="primary" className="me-1">Submit</Button>
@@ -83,9 +80,24 @@ export const PairingsReport = () => {
         <Card.Body>
           <Card.Title>Items that sold together:</Card.Title>
           <Card.Text>
-          {Item1.map(item1 => (
-            <ol key={item1.frequency}> {item1.item_1} & {item1.item_2}: {item1.frequency}</ol>
-          ))}
+            <Table striped bordered hover responsive>
+              <thead>
+                <tr>
+                  <th>First Item</th>
+                  <th>Second Item</th>
+                  <th>Frequency</th>
+                </tr>
+              </thead>
+              <tbody>
+                {Item1.map(item1 => (
+                  <tr key={item1.frequency}>
+                    <td>{item1.item_1}</td>
+                    <td>{item1.item_2}</td>
+                    <td>{item1.frequency}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
           </Card.Text>
           <Link to={"/ExtraFeatures"}><Button variant="primary">Done</Button></Link>
         </Card.Body>
